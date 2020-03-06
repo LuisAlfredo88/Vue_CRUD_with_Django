a4172c5c1c7172bd1acf57fb4e8957b13c2cd630
ldilone <luisalfredodilone@gmail.com>
2020-03-05 23:38:17 -0400

    Removing dummy data

--- vue_project/src/firebase/index.js
+++ vue_project/src/firebase/index.js
@@ -4,8 +4,8 @@ import api from '@/store/api'
 
 const showNotification = (message) => {
     new Notification(
-        'Control de Pago',
-        {icon: 'https://deivisadames.com/payment/img/logo.abbbd621.png', 
+        'App-name',
+        {icon: 'icon-for-notification', 
             body: message,
             dir : "ltr",
             requireInteraction: true
@@ -25,8 +25,8 @@ const startFirebase = () => {
     //     databaseURL: "https://my-project-83717.firebaseio.com",
     //     projectId: "my-project-83717",
     //     storageBucket: "my-project-83717.appspot.com",
-    //     messagingSenderId: "392021395277",
-    //     appId: "1:392021395277:web:a0b4e114035c0fd5509904"        
+    //     messagingSenderId: "mjs-id",
+    //     appId: "app-key"        
     // });
 
     // const messaging = firebase.messaging();
@@ -46,42 +46,6 @@ const startFirebase = () => {
     //     console.log(message);
     //     showNotification(message);
     // });
-
-
-    // navigator.serviceWorker.register('./static/firebase-messaging-sw.js')
-    // .then((registration) => {
-    //     firebase.messaging.useServiceWorker(registration)
-    //     firebase.messaging.requestPermission().then(() => {
-    //         firebase.messaging.getToken().then((token) => {
-    //             console.log(token);
-    //         }).catch((err) => console.log(err))
-    //     }).catch(function (err) {
-    //         console.log('Unable to get permission to notify.', err)
-    //     })
-    // }).catch(err => {
-    //     console.log('error register', err)
-    // })
-
-    // const messaging = firebase.messaging();
-
-    // messaging.getToken().then((currentToken) => {
-    //     console.log(currentToken);
-    //     api.commons.setFirebaseToken(currentToken);
-    // }).catch((err) => {});
-
-    // messaging.onTokenRefresh(() => {
-    //     messaging.getToken().then((refreshedToken) => {
-    //         api.commons.setFirebaseToken(refreshedToken);
-    //     }).catch((err) => {});
-    // });
-
-    // messaging.onMessage((message) => {
-    //     console.log(message);
-    //     showNotification(message);
-    // });
-
-
-
 }
 
 const startMessaging = () => {
